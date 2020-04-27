421290cf70fb4f04681f32242858d82087ad587c
Pierre jonny cau <jonny.cau@cynny.com>
2020-04-27 10:48:32 +0200

    start project

---
+++ POC_C-MVVM-RX/POC_C-MVVM-RX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC_C-MVVM-RX
+//
+//  Created by Pierre jonny cau on 27/04/2020.
+//  Copyright © 2020 Pierre Jonny Cau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ POC_C-MVVM-RX/POC_C-MVVM-RXTests/POC_C_MVVM_RXTests.swift
@@ -0,0 +1,34 @@
+//
+//  POC_C_MVVM_RXTests.swift
+//  POC_C-MVVM-RXTests
+//
+//  Created by Pierre jonny cau on 27/04/2020.
+//  Copyright © 2020 Pierre Jonny Cau. All rights reserved.
+//
+
+import XCTest
+@testable import POC_C_MVVM_RX
+
+class POC_C_MVVM_RXTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
